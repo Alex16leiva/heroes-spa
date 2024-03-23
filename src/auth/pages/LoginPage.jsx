8fa48df2c736c97d4d66445c93d96d946ba7173a
alex16leiva <alex16leiva@gmail.com>
2024-03-23 07:30:31 -0600

    modificacion login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom"
 import { AuthContext } from "../context/AuthContext";
 
 export const LoginPage = () => {
+  
   const { login } =  useContext( AuthContext );
   const navigate = useNavigate();
 
